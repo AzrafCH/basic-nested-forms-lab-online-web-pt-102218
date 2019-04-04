be2eb51bfcc47bef5cee13e0e2592c1f4016a949
Azraf Chowdhury <achowdhur@ltu.edu>
2019-04-04 22:47:31 +0000

    Automatically backed up by Learn

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -21,7 +21,7 @@ class RecipesController < ApplicationController
   private
 
   def recipe_params
-    params.require(:recipe).permit(:name, 
+    params.require(:recipe).permit(:name,
     ingredients_attributes: [
       :name,
       :quantity
